5b849ec208b6ad69dfdbec60832a196d99b8dea7
nopo <noahogno@gmail.com>
2022-08-28 16:06:41 +1000

    Added 1.12.2 support

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -9,8 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
-		// Below is a demonstration of an access-transformed class access.
-        System.out.println("Color State: " + new GlStateManager.Color());
+        System.out.println("Dirt: " + Blocks.DIRT.getTranslationKey());
+	    System.out.println("Color State: " + new GlStateManager.Color());
     }
 }
